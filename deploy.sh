7d66a64a703102bd9b3924421cf15e19c13cb7a8
Don Blair <donblair@gmail.com>
2020-04-02 09:41:10 -0400

    update

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
-cp -r _site/* ~/gitwork/mosspig.github.io/ 
-
+git add *
+git commit -m 'update'
+git push
+cp -r _site/* ../mosspig.github.io/
+cd ../mosspig.github.io/
+git add *	
+git commit -m 'update'
+git push
